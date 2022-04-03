ecac6d0b267539f0f9e577a8ef8470b1d416f28a
LuluLePerdu <99348300+LuluLePerdu@users.noreply.github.com>
2022-04-03 01:08:05 -0400

    Update TP1-3.sh

--- Bash TP1/TP1-3.sh
+++ Bash TP1/TP1-3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#En bash, créer un script qui parcourt tous les fichiers txt d'un répertoire donné en argument 
-#et qui affiche les 10 premières lignes de chacun de ces fichiers de façon suivante dans le fichier entêtes.txt:
+#Description : Script qui parcourt tous les fichiers txt d'un répertoire donné en argument qui affiche les 10 premières lignes de chacun de ces fichiers.
+#Auteur : Ludwig-Emmanuel Dufour
 
 echo "Entrez le répertoire que vous désirez parcourir:"
 read repertoire
@@ -8,9 +8,10 @@ cd $repertoire
 
     for fichier in *.txt
     do  
-        head -v $fichier >>entête.txt
-        #sed -n '1,10p' $fichier >> entête.txt
+        echo -e "\n${fichier##*/}:" >> ./entête.txt
         echo "">>entête.txt
+        head -10 $fichier >> ./entête.txt
+      
     done
 echo "Toute les 10 premières lignes ont été copiés dans entête.txt"
 sleep 4
