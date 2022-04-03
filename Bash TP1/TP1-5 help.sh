a919bcf8b1cd1650810c213c0bd2b77b995eb68f
LuluLePerdu <99348300+LuluLePerdu@users.noreply.github.com>
2022-04-03 01:14:56 -0400

    Update TP1-5 help.sh

--- Bash TP1/TP1-5 help.sh
+++ Bash TP1/TP1-5 help.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-ls --sort=size -s1 $(find -type f) > log.txt
+#Description : Inscrire en ordre décroissant, le nom et la taille des fichiers dans le répertoire et les sous-répertoires.
+#Auteur : Ludwig-Emmanuel Dufour
+
+ls --sort=size -s1 $(find -type f)
