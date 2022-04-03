0db44cf602bff82bd6d3023da475235cac64900a
LuluLePerdu <99348300+LuluLePerdu@users.noreply.github.com>
2022-04-03 01:11:55 -0400

    Update TP1-4.sh

--- Bash TP1/TP1-4.sh
+++ Bash TP1/TP1-4.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Description : Compare des nombres aléatoires pour voir si un id de processus est simmilaire.
+#Auteur : Ludwig-Emmanuel Dufour
 
 declare -a tab
 
@@ -19,6 +21,4 @@ for i in ${tab[@]}; do
         echo "$i n'est pas un processus"
 	fi
 done
-
-sleep 20
 
