6c6508133f72f926750d3354ee1ed381c28ab7b1
LuluLePerdu <99348300+LuluLePerdu@users.noreply.github.com>
2022-04-03 01:28:06 -0400

    Update TP1-2.ps1

--- PowerShell TP1/TP1-2.ps1
+++ PowerShell TP1/TP1-2.ps1
@@ -1,5 +1,9 @@
-
-
+<#
+Description : Voir la liste des logs d’application qui se situe entre les deux dates (debut et fin) passées par aguments et 
+retourner les résultats dans un fichier nommé sortie.csv (sous le format csv).
+Auteur : Ludwig-Emmanuel Dufour
+#> 
+ 
 [CmdletBinding()]
 Param(
   [Parameter(Mandatory=$true)]
@@ -9,4 +13,3 @@ Param(
 )
 
 Get-EventLog Application -After $dateDebut -Before $dateFin | ConvertTo-Csv | Out-File .\sortie.csv
-pause
