0242b4d124b01c90afe83eac6c5470f7efedb12d
LuluLePerdu <99348300+LuluLePerdu@users.noreply.github.com>
2022-04-04 23:20:49 -0400

    Update TP1-3.ps1

--- PowerShell TP1/TP1-3.ps1
+++ PowerShell TP1/TP1-3.ps1
@@ -1,6 +1,11 @@
 <# 
+Filename: TP1-3.ps1
 Description : Plus de détail (nom, id, taille en mémoire centrale, nombre de threads) sur un processus passé en ligne de commande.
+Argument: Aucun
+Variable modifiable: Le processus demandé.
 Auteur : Ludwig-Emmanuel Dufour
+Entré: Le processus
+Sortie: Les infos du processus
 #> 
 
 [CmdletBinding()]
