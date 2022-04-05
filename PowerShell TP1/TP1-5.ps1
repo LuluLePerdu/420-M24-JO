b96ed59961dcf032ecae5792ea58860074565f1a
LuluLePerdu <99348300+LuluLePerdu@users.noreply.github.com>
2022-04-04 23:24:17 -0400

    Update TP1-5.ps1

--- PowerShell TP1/TP1-5.ps1
+++ PowerShell TP1/TP1-5.ps1
@@ -1,6 +1,9 @@
 <#
+Filename: TP1-5.ps1
 Description : Rechercher dans le répertoire en cours et enfant tous les fichiers et inscrire en ordre décroissant, le nom et la taille du fichier.
 Auteur : Ludwig-Emmanuel Dufour
+Argument: Aucun.
+Variable modifiable: Aucune.
 #>
 
 Get-ChildItem . -Recurse -File | Sort-Object -Descending Length | Select-Object Length,Name
