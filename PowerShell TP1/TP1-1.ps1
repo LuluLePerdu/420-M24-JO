253b7ce8053a0d5b131dddfc7234766b33fdd2c3
LuluLePerdu <99348300+LuluLePerdu@users.noreply.github.com>
2022-04-04 23:19:00 -0400

    Update TP1-1.ps1

--- PowerShell TP1/TP1-1.ps1
+++ PowerShell TP1/TP1-1.ps1
@@ -1,7 +1,7 @@
 <#
 Filename: TP1-1.ps1
-Description:
-Argument:
+Description:script qui parcourt tous les fichiers txt d'un répertoire donné en argument et qui affiche les 10 premières lignes de chacun de ces fichiers.
+Argument: Répertoire a parcourir
 Auteur: Ludwig-Emmnanuel Dufour
 Variable modifiable: Aucune
 Sortie: Les 10 premières lignes d'un fichier texte
@@ -12,5 +12,5 @@ $rep = read-host -Prompt "Entrez le répertoire que vous désirez parcourir"
 $files = Get-ChildItem $rep -Filter *txt -Name
 foreach($file in $files){
     Write-Output "'n$file'"
-    Get-Content $rep/$file -head 10
+    Get-Content $rep/$file -head 10 > entete.txt
 }
