873cbf8f938a94b74dcfe462745a44cfa1774405
LuluLePerdu <99348300+LuluLePerdu@users.noreply.github.com>
2022-04-04 23:09:44 -0400

    Update TP1-1.ps1

--- PowerShell TP1/TP1-1.ps1
+++ PowerShell TP1/TP1-1.ps1
@@ -1,10 +1,12 @@
 <#
-
-
-
-
-
+Filename: TP1-1.ps1
+Description:
+Argument:
+Auteur: Ludwig-Emmnanuel Dufour
+Variable modifiable: Aucune
+Sortie: Les 10 premières lignes d'un fichier texte
 #>
+
 $rep = read-host -Prompt "Entrez le répertoire que vous désirez parcourir"
 
 $files = Get-ChildItem $rep -Filter *txt -Name
