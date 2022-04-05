92cbad919850d9afc3a0fd9d07d141be7eb826db
LuluLePerdu <99348300+LuluLePerdu@users.noreply.github.com>
2022-04-04 23:12:00 -0400

    Update TP1-2.ps1

--- PowerShell TP1/TP1-2.ps1
+++ PowerShell TP1/TP1-2.ps1
@@ -1,7 +1,11 @@
 <#
+Filename: TP1-2.ps1
 Description : Voir la liste des logs d’application qui se situe entre les deux dates (debut et fin) passées par aguments et 
 retourner les résultats dans un fichier nommé sortie.csv (sous le format csv).
+Arguments: La date de Début et date de Fin.
 Auteur : Ludwig-Emmanuel Dufour
+Variable modifiable: Les deux dates.
+Sortie: Fichier .csv contenant les logs d'applications entre les deux dates.
 #> 
  
 [CmdletBinding()]
