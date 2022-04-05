78233bec0f3e0c9dffe9f1b061b0ab9e06426545
LuluLePerdu <99348300+LuluLePerdu@users.noreply.github.com>
2022-04-04 23:27:05 -0400

    Update TP1-6.ps1

--- PowerShell TP1/TP1-6.ps1
+++ PowerShell TP1/TP1-6.ps1
@@ -1,7 +1,10 @@
 <#
+Filename: TP1-6.ps1
 Description : Lister les disques, l'espace disponible, l'espace utilisé, le pourcentage d'utilisation et la capacité maximale totale pour chacun d'eux.
 Exporter le résultat dans un fichier HTML nommé fichier.html.
+Arguments: Aucun
 Auteur : Ludwig-Emmanuel Dufour
+Variable modifiable: Aucune
 #>
 
 Get-PsDrive | Select-Object Name,Free,@{Name = "Total"; Expression = {$_.free + $_.used}},@{Name="Pourcentage"; Expression = {$_.used / ($_.free + $_.used) * 100}} | ConvertTo-Html | Out-File fichier.html
