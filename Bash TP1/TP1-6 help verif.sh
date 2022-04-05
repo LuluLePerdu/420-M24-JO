57db260a641cd13f58ae5b2ee887db2201c33b5f
LuluLePerdu <99348300+LuluLePerdu@users.noreply.github.com>
2022-04-04 22:20:37 -0400

    Update TP1-6 help verif.sh

--- Bash TP1/TP1-6 help verif.sh
+++ Bash TP1/TP1-6 help verif.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-    echo "entrez le repertoire"
-    read $1
-    echo "entrez la taille de l'image"
-    read $2
-    echo "Entrez le repertoire des nouvelles images"
-    read $3
-
-    cd $1
 
     for i in $1/*; do
         if [[ $i == *.png || $i == *.gif || $i == *.jpg ]]; then
